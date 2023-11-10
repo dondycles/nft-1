909a65a95b41a359697834ddbd3e10c9203bf3a6
dondycles <johnrod.dondoyano@gmail.com>
2023-11-10 15:12:52 +0800

    modified:   pages/index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,10 @@ export default function Home() {
   return (
     <main className="pb-32 px-4 sm:px-16 md:px-32 lg:px-64 max-h-full w-full pt-16 grid grid-cols-1 sm:grid-cols-2 2xl:grid-cols-4  gap-2 overflow-y-auto overflow-x-hidden">
       {Array.from({ length: 8 }, (_, index) => (
-        <div className=" rounded-xl bg-primary/10  p-2 flex flex-col gap-2  aspect-square">
+        <div
+          key={index}
+          className=" rounded-xl bg-primary/10  p-2 flex flex-col gap-2  aspect-square"
+        >
           <div className="bg-black rounded-lg h-full  aspect-square"></div>
           <p className="text-xs text-center font-black">
             Lorem ipsum dolor sit.
